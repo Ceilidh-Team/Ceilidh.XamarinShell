584ba14de72cfef12f0d7b3d06c217dd97789bba
Olivia Trewin <zancoproductions@gmail.com>
2018-09-13 18:20:00 -0500

    Added basic bootstrap code
    This includes the Mac target, the shared UI component, and a reference to Ceilidh.Standard

---
+++ ProjectCeilidh.Ceilidh.XamarinShell.Mac/AppDelegate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace ProjectCeilidh.Ceilidh.XamarinShell.Mac
+{
+    [Register("AppDelegate")]
+    public sealed class AppDelegate : FormsApplicationDelegate
+    {
+        public override NSWindow MainWindow { get; }
+
+        public AppDelegate()
+        {
+            MainWindow = new NSWindow(new CoreGraphics.CGRect(200, 1000, 1024, 768), NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled | NSWindowStyle.Miniaturizable, NSBackingStore.Buffered, false)
+            {
+                Title = "Ceilidh",
+                TitleVisibility = NSWindowTitleVisibility.Visible,
+
+            };
+        }
+
+        public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender) => true;
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        public static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate();
+            NSApplication.Main(args);
+        }
+    }
+}
