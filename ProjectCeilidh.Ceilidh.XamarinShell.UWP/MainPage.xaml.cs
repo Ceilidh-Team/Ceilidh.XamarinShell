f4814d7ac5d8b53087ae5c3e78bcf6edec7ec272
Olivia Trewin <trewinolivia@gmail.com>
2018-09-13 22:22:03 -0500

    Added UWP and WPF targets

---
+++ ProjectCeilidh.Ceilidh.XamarinShell.UWP/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace ProjectCeilidh.Ceilidh.XamarinShell.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new XamarinShell.App());
+        }
+    }
+}
---
+++ ProjectCeilidh.Ceilidh.XamarinShell.WPF/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace ProjectCeilidh.Ceilidh.XamarinShell.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            LoadApplication(new XamarinShell.App());
+        }
+    }
+}
