ef15b208b1f247a81c1cd2483af00ba5280ae024
Olivia Trewin <trewinolivia@gmail.com>
2018-09-15 14:29:31 -0500

    Add GTK project

---
+++ ProjectCeilidh.Ceilidh.XamarinShell.GTK/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace ProjectCeilidh.Ceilidh.XamarinShell.GTK
+{
+    class MainClass
+    {
+		[STAThread]
+        public static void Main(string[] args)
+        {
+			Gtk.Application.Init();
+			Forms.Init();
+   
+
+            var window = new FormsWindow();
+			window.LoadApplication(new XamarinShell.App());
+			window.SetApplicationTitle("Ceilidh");
+			window.Show();
+
+			Gtk.Application.Run();
+        }
+    }
+}
