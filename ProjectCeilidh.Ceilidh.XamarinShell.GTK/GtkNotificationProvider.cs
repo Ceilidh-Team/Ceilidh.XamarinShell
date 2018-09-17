2880b0de6fb9dfd1eba47d395672533b49e4938b
Olivia Trewin <zancoproductions@gmail.com>
2018-09-17 18:05:04 -0500

    Improve interface with libnotify

--- ProjectCeilidh.Ceilidh.XamarinShell.GTK/GtkNotificationProvider.cs
+++ ProjectCeilidh.Ceilidh.XamarinShell.GTK/GtkNotificationProvider.cs
@@ -6,9 +6,17 @@ namespace ProjectCeilidh.Ceilidh.XamarinShell.GTK
 {
     public class GtkNotificationProvider : INotificationProvider
     {
+        private static readonly NotifyActionCallback<GtkNotificationProvider> NotifyActionCallback = (notification, action, provider) =>
+        {
+            if (action != "default") return;
+
+            provider.Action?.Invoke(provider, new NotificationActionEventArgs());
+        };
+
         public void DisplayNotification(string identifier, string title, string text)
         {
             var notify = new LibNotify.NotifyNotification(title, text, "dialog-information");
+            notify.AddAction("default", null, NotifyActionCallback, this);
             if (!notify.Show(out var error))
             {
                 var err = Marshal.PtrToStructure<GError>(error);
--- ProjectCeilidh.Ceilidh.XamarinShell.GTK/LibNotify/NotifyNotification.cs
+++ ProjectCeilidh.Ceilidh.XamarinShell.GTK/LibNotify/NotifyNotification.cs
@@ -3,8 +3,29 @@ using System.Runtime.InteropServices;
 
 namespace ProjectCeilidh.Ceilidh.XamarinShell.GTK.LibNotify
 {
+    public delegate void NotifyActionCallback<T>(NotifyNotification notifyNotification, string action, T userData);
+
     public class NotifyNotification
     {
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate void GFreeFunc(IntPtr data);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private delegate void NotifyActionCallback(IntPtr notificationPtr, string action, IntPtr opaque);
+
+        private static readonly GFreeFunc GCHandleFreeFuncDelegate = ptr =>
+        {
+            if (ptr == IntPtr.Zero) return;
+
+            GCHandle.FromIntPtr(ptr).Free();
+        };
+
+        private static readonly NotifyActionCallback NotifyActionCallbackDelegate = (notificationPtr, action, opaque) =>
+        {
+            var handle = (UserData) GCHandle.FromIntPtr(opaque).Target;
+            handle.Callback.DynamicInvoke(new NotifyNotification(notificationPtr), action, handle.Data);
+        };
+
         public static bool IsInitted => NotifyIsInitted();
 
         public static extern string AppName
@@ -17,15 +38,36 @@ namespace ProjectCeilidh.Ceilidh.XamarinShell.GTK.LibNotify
 
         private readonly IntPtr _notificationPtr;
 
-        public NotifyNotification(string summary, string body, string icon)
+        public NotifyNotification(string summary, string body, string icon) : this(NotifyNotificationNew(summary, body, icon))
+        {
+        }
+
+        private NotifyNotification(IntPtr notificationPtr)
         {
             if (!IsInitted && !NotifyInit("hiya"))
                 throw new Exception("Notify did not init"); // ayy lmao
 
-            _notificationPtr = NotifyNotificationNew(summary, body, icon);
+            _notificationPtr = notificationPtr;
         }
 
         public bool Show(out IntPtr error) => NotifyNotificationShow(_notificationPtr, out error);
+
+        public void AddAction<T>(string action, string label, NotifyActionCallback<T> callback, T userData)
+        {
+            NotifyNotificationAddAction(_notificationPtr, action, label, NotifyActionCallbackDelegate, GCHandle.ToIntPtr(GCHandle.Alloc(new UserData(callback, userData))), GCHandleFreeFuncDelegate);
+        }
+
+        private class UserData
+        {
+            public readonly Delegate Callback;
+            public readonly object Data;
+
+            public UserData(Delegate callback, object userData)
+            {
+                Callback = callback;
+                Data = userData;
+            }
+        }
 
         #region Native
 
@@ -40,6 +82,9 @@ namespace ProjectCeilidh.Ceilidh.XamarinShell.GTK.LibNotify
 
         [DllImport("libnotify", EntryPoint = "notify_notification_show")]
         private static extern bool NotifyNotificationShow(IntPtr notificationPtr, out IntPtr error);
+
+        [DllImport("libnotify", EntryPoint = "notify_notification_add_action", CharSet = CharSet.Ansi)]
+        private static extern void NotifyNotificationAddAction(IntPtr notificationPtr, string action, string label, NotifyActionCallback callback, IntPtr opaque, GFreeFunc freeFunc);
 
         #endregion
     }
