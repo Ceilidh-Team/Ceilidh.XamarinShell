7e32c64fc52b0c8c72b02715efbe392f73dae046
Olivia Trewin <trewinolivia@gmail.com>
2018-09-18 17:35:30 -0500

    Wpf notifications? Maybe?

--- ProjectCeilidh.Ceilidh.XamarinShell.WPF/WpfNotificationProvider.cs
+++ ProjectCeilidh.Ceilidh.XamarinShell.WPF/WpfNotificationProvider.cs
@@ -1,37 +1,19 @@
 ﻿using System;
-using System.Linq;
-using Windows.ApplicationModel;
 
 namespace ProjectCeilidh.Ceilidh.XamarinShell.WPF
 {
     public class WpfNotificationProvider : INotificationProvider
     {
-        public NotificationHandle CreateNotification()
+        public void DisplayNotification(string identifier, string title, string text)
         {
-            return new WpfNotificationHandle();
+            throw new NotImplementedException();
         }
 
-        private class WpfNotificationHandle : NotificationHandle
+        public void RequestUserAttention()
         {
-            public override string Title { get; set; }
-            public override string Text { get; set; }
-
-            public WpfNotificationHandle()
-            {
-
-            }
-
-            public override void CreateAction(string label)
-            {
-            }
-
-            public override void Show()
-            {
-                var id = Package.Current.GetAppListEntriesAsync().AsTask().Result.First().AppUserModelId;
-                var a = "ProjectCeilidh.Ceilidh.XamarinShell.WPF!App";
-            }
-
-            public override event NotificationActionEventHandler NotificationAction;
+            throw new NotImplementedException();
         }
+
+        public event NotificationActionEventHandler Action;
     }
 }
